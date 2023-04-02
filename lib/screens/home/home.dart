409ca3805ca7d002b5a7dd1e7ad3513d21b5f4d7
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-04-03 00:43:42 +0800

    Making of 3 objects and provider

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,11 +3,15 @@ import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
 class Home extends StatelessWidget{
-  
+
   const Home({super.key});
 
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+    );
   }
 }
---
+++ lib/screens/home/providers/object_provider.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:detailsofprovider_course/screens/home/classes/base_object.dart';
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+class ObjectProvider extends ChangeNotifier{
+  late String id;
+  late CheapObject _cheapObject;
+  late StreamSubscription _cheapObjectSubscription;
+  late ExpensiveObject _expensiveObject;
+  late StreamSubscription _expensiveObjectSubscription;
+
+  CheapObject get cheapObject => _cheapObject;
+  ExpensiveObject get expensiveObject => _expensiveObject;
+
+  ObjectProvider() :
+        id = const Uuid().v4(),
+        _cheapObject = CheapObject(),
+        _expensiveObject = ExpensiveObject();
+
+  @override
+  void notifyListeners(){
+    id = const Uuid().v4();
+    super.notifyListeners();
+  }
+
+  void start(){
+    _cheapObjectSubscription = Stream.periodic(
+      const Duration(seconds: 1),
+    ).listen((_) {
+      _cheapObject = CheapObject();
+      notifyListeners();
+    });
+    _expensiveObjectSubscription = Stream.periodic(
+      const Duration(seconds: 10),
+    ).listen((_) {
+      _expensiveObject = ExpensiveObject();
+      notifyListeners();
+    });
+  }
+
+  void stop(){
+    _cheapObjectSubscription.cancel();
+    _expensiveObjectSubscription.cancel();
+  }
+}
