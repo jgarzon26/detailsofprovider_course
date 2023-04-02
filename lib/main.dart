a4a92e5ceb76311d7ae2dae896d5be3b7eafb7a1
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-04-02 22:41:42 +0800

    Init project

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,15 @@
+import 'package:detailsofprovider_course/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
+
+class Home extends StatelessWidget{
+  
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
